1a672600b552fab314bd1e16212e8850ecd8b9a3
ned <nedschultz@outlook.com>
2023-01-12 15:00:24 +1000

    finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+
+    const formElement = document.querySelector("form")
+    formElement.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const searchByIDField = document.getElementById("searchByID");
+        const id = searchByIDField.value;
+
+        fetch(`http://localhost:3000/movies/${id}`)
+            .then(res => res.json())
+            .then((movie) => {
+                const titleElement = document.querySelector("h4");
+                const summaryElement = document.querySelector("p");
+
+                titleElement.innerHTML = movie.title;
+                summaryElement.innerHTML = movie.summary;
+            });
+    });
+
+};
 
 document.addEventListener('DOMContentLoaded', init);
